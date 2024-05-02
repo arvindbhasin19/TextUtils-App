4fa17af8e96987ddde3d895a7aa07f3e031d058b
Arvind Singh <arvindbhasin4994@gmail.com>
2024-05-02 15:22:28 +0530

    TextUtils App
    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { PropTypes } from "prop-types"
+import { Link } from "react-router-dom"
+
+export default function Navbar(props) {
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            <div className="container-fluid">
+                <Link className={`navbar-brand text-${props.mode==='light'?'dark':'light'}` } to="/">{props.logo}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className={`nav-link active text-${props.mode==='light'?'dark':'light'}`} aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={`nav-link text-${props.mode==='light'?'dark':'light'}`} to="/about">{props.title}</Link>
+                        </li>
+
+
+                    </ul>
+                    {/* <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form> */}
+                    <div className="form-check form-switch">
+                        <input className="form-check-input" type="checkbox" onClick={props.greenMode} role="switch" id="flexSwitchCheckDefault" />
+                        {/* <label className={`form-check-label text-${(props.mode==='light'?'success':'light')}`} htmlFor="flexSwitchCheckDefault">{props.mode==='success'?'Enable LightMode':'Enable Green Theme'}</label> */}
+                    </div>
+                    <div className="form-check form-switch">
+                        <input className="form-check-input" type="checkbox" onClick={props.modeControl} role="switch" id="flexSwitchCheckDefault" />
+                        <label className={`form-check-label text-${(props.mode==='light'?'dark':'light')}`} htmlFor="flexSwitchCheckDefault">{(props.mode==='dark' || props.mode==='success')?'Enable LightMode':'Enable DarkMode'}</label>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.propTypes = {
+    logo: PropTypes.string.isRequired,
+    title: PropTypes.string,
+}
+
+Navbar.defaultProps = {
+    logo: 'Set Logo here'
+};
---
+++ src/components/Textform.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+export default function Textform(props) {
+    // for uppercase
+    const toUppercae = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert('success', 'Converted into uppercase')
+    }
+
+    // for lowecase
+    const toLowercase = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert('success', 'Converted into lowercase')
+    }
+
+    //handle Onchange 
+    const handleOnChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const clearText = ()=>{
+        let newText = '';
+        setText(newText);
+        props.showAlert('success', 'Text Removed')
+    }
+
+    const [text, setText] = useState('');
+    return (
+       <>
+        <div className='container'>
+            <h2 className={`text-${props.mode==='light'?'dark':'light'}`}>{props.heading}</h2>
+            <div className="mb-3">
+                <textarea className={`form-control text-${(props.mode==='dark' || props.mode==='success')?'light':'dark'}`} style={{backgroundColor: (props.mode==='dark' || props.mode==='success')?'rgb(128 125 125)':'#fff'}} value={text} id="textBox" onChange={handleOnChange} rows="8"></textarea>
+            </div>
+            <button className={`btn btn-${props.mode==='success'?'success':'primary'} btn-sm mx-2`} onClick={toUppercae} >Convert To Uppercase</button>
+            <button className={`btn btn-${props.mode==='success'?'success':'primary'} btn-sm mx-2`} onClick={toLowercase} >Convert To Uppercase</button>
+            <button className={`btn btn-${props.mode==='success'?'success':'primary'} btn-sm mx-2`} onClick={clearText} >Clear Text</button>
+        </div>
+        <div className="container my-2">
+            <h2 className={`text-${props.mode==='light'?'dark':'light'}`} >Text Summary</h2>
+            <p className={`text-${props.mode==='light'?'dark':'light'}`} >{(text.length === 0)?(0):text.length} characters {(text.length === 0)?(0):text.split(' ').length} words</p>
+            <p className={`text-${props.mode==='light'?'dark':'light'}`} >{((text.length>0)?(0.008 * text.split(' ').length):(0 * text.split(' ').length))} Mintues to read</p>
+            <h2 className={`text-${props.mode==='light'?'dark':'light'}`} >Preview</h2>
+            <p className={`text-${props.mode==='light'?'dark':'light'}`} >{text.length>0?text:'Enter something to preview it here'}</p>
+        </div>
+        </>
+    )
+}

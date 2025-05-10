1f6d40e129d45d0c818719c499520d0ec5acbc30
arvindbhasin19 <arvindbhasin4994@gmail.com>
2025-05-10 22:29:44 +0530

    make responsive in mobile

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,7 +9,7 @@ export default function AboutUs(props) {
 
     return (
         <>
-            <div className="container" >
+            <div className="container pb-4">
                 <h1 className={`text-${(props.mode === 'light') ? 'dark' : 'light'}`}>About Us</h1>
                 <p className={`text-${(props.mode === 'light') ? 'dark' : 'light'}`}>
                     TextUtils is a ReactJs website built primarily to do various operations on regular typed text. You can safely use WordPad or NotePad for text drafting, and saving, but TextUtils offers much more than simple text drafting and formatting. TextUtils can convert your text to any case in just one simple click of a button. It can extract links and numbers safely from a labyrinth of random text or sophisticated documentation. It has an improved property of base64 encoding, reversal of your inputted text.You can even remove whitespaces from your scripted documents, and wear up your earphones to listen to it, instead of straining your eyes! It does a detailed analyzing of your text, and provides an output of words, and characters, along with reading time as well. The best part of TextUtils is that it is an open source project, as a result, dozens of new features are in the upcoming, which definitely makes it a cut above the rest.
